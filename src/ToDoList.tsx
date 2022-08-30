9916d95e8ec59b52af28c8c782ab299c2ec559fc
June Roh <june.roh@streetsmarket.com>
2022-08-30 15:51:11 -0400

    get used to react hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,19 +1,109 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IFormData {
+    email: string;
+    firstname: string;
+    lastname: string;
+    username: string;
+    password: string;
+    password1: string;
+    extraError?: string;
+}
+
 function ToDoList() {
-    const { register, watch } = useForm();
-    console.log(watch());
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setError,
+    } = useForm<IFormData>({
+        defaultValues: {
+            email: "@naver.com",
+        },
+    });
+    const onValid = (data: IFormData) => {
+        if (data.password !== data.password1) {
+            setError(
+                "password1",
+                { message: "Password are not the same" },
+                { shouldFocus: true }
+            );
+        }
+        // setError("extraError", { message: "Server offline." });
+    };
+    console.log(errors);
+
     return (
         <div>
-            <form>
-                <input {...register("email")} placeholder="Email" />
-                <input {...register("firstname")} placeholder="First Name" />
-                <input {...register("lastname")} placeholder="Last Name" />
-                <input {...register("username")} placeholder="Username" />
-                <input {...register("password")} placeholder="Password" />
-                <input {...register("password1")} placeholder="Password1" />
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit(onValid)}
+            >
+                <input
+                    {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+                            message: "Only naver.com emails allowed",
+                        },
+                    })}
+                    placeholder="Email"
+                />
+                <span>{errors?.email?.message}</span>
+                <input
+                    {...register("firstname", {
+                        required: "First name is required",
+                        validate: {
+                            noAdmin: (value) =>
+                                value.includes("admin")
+                                    ? "'Admin' can't be used"
+                                    : true,
+                            noJune: (value) =>
+                                value.includes("june")
+                                    ? "june is not allowed"
+                                    : true,
+                        },
+                    })}
+                    placeholder="First Name"
+                />
+                <span>{errors?.firstname?.message}</span>
+                <input
+                    {...register("lastname", {
+                        required: "Last name is required",
+                    })}
+                    placeholder="Last Name"
+                />
+                <span>{errors?.lastname?.message}</span>
+                <input
+                    {...register("username", {
+                        required: "Username is required",
+                        minLength: 10,
+                    })}
+                    placeholder="Username"
+                />
+                <span>{errors?.username?.message}</span>
+                <input
+                    {...register("password", {
+                        required: "Password is required",
+                        minLength: 5,
+                    })}
+                    placeholder="Password"
+                />
+                <span>{errors?.password?.message}</span>
+                <input
+                    {...register("password1", {
+                        required: "Password is required",
+                        minLength: {
+                            value: 5,
+                            message: "Your password is too short.",
+                        },
+                    })}
+                    placeholder="Password1"
+                />
+                <span>{errors?.password1?.message}</span>
                 <button>Add</button>
+                <span>{errors?.extraError?.message}</span>
             </form>
         </div>
     );
